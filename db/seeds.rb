a1f461825f48e5d43d84b887c251d1bcefb47800
gurmukhsingh <gurmukhs300@gmail.com>
2022-11-11 12:29:10 +1100

    seeding with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -17,3 +18,14 @@ pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6
   puts "Created #{restaurant.name}"
 end
 puts "Finished!"
+
+puts 'Faker data time!'
+25.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    rating: rand(1..5)
+  )
+  restaurant.save
+end
+puts "All Done"
